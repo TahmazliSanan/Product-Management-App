62fdac0f1598287c63e2b3699b5ceb2629558e2c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-11 22:41:33 +0400

    Create CategoryDal

---
+++ ProductManagementApp/DataAccess/Helpers/CategoryDal.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace ProductManagementApp.DataAccess.Helpers
+{
+    public static partial class Dal
+    {
+        public static void FillCategories(this DataTable table)
+        {
+            using (IDbCommand command = Connection.CreateCommand())
+            {
+                command.CommandText = "select id, name from categories";
+
+                if (command.Connection.State != ConnectionState.Open)
+                {
+                    command.Connection.Open();
+                }
+
+                IDataReader reader = command.ExecuteReader();
+                table.Load(reader);
+            }
+        }
+    }
+}
