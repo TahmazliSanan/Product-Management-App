3a1764d7420694869faa49093da557551ce39342
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-11 22:40:03 +0400

    Add method: btnMenuCategory_Click

--- ProductManagementApp/Forms/MainForm.Designer.cs
+++ ProductManagementApp/Forms/MainForm.Designer.cs
@@ -42,9 +42,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(276, 40);
+            this.dataGridView1.Location = new System.Drawing.Point(276, 27);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(555, 418);
+            this.dataGridView1.Size = new System.Drawing.Size(555, 434);
             this.dataGridView1.TabIndex = 0;
             // 
             // menuBar
@@ -70,6 +70,7 @@
             this.btnMenuCategory.Name = "btnMenuCategory";
             this.btnMenuCategory.Size = new System.Drawing.Size(180, 22);
             this.btnMenuCategory.Text = "Categories";
+            this.btnMenuCategory.Click += new System.EventHandler(this.btnMenuCategory_Click);
             // 
             // MainForm
             // 
@@ -82,6 +83,7 @@
             this.MainMenuStrip = this.menuBar;
             this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Product Management App";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuBar.ResumeLayout(false);
--- ProductManagementApp/Forms/MainForm.cs
+++ ProductManagementApp/Forms/MainForm.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ProductManagementApp.Forms;
+using System;
 using System.Windows.Forms;
 
 namespace ProductManagementApp
@@ -15,6 +9,12 @@ namespace ProductManagementApp
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnMenuCategory_Click(object sender, EventArgs e)
+        {
+            CategoryForm categoryForm = new CategoryForm();
+            categoryForm.Show(this);
         }
     }
 }
