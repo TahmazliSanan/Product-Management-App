2e50e422b5f15484db3a9255174e892847b939bb
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-11 22:41:14 +0400

    Create Generic Data Access Layer

---
+++ ProductManagementApp/DataAccess/Helpers/GenericDal.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProductManagementApp.DataAccess.Helpers
+{
+    public static partial class Dal
+    {
+        static MySqlConnection connection = null;
+        static object lockObject = new object();
+
+        public static IDbConnection Connection
+        {
+            get
+            {
+                if (connection is null)
+                {
+                    lock (lockObject)
+                    {
+                        if (connection is null)
+                        {
+                            connection = new MySqlConnection("Server=localhost;Database=product_management_db;Uid=root;Pwd=23042002;");
+                        }
+                    }
+                }
+
+                return connection;
+            }
+        }
+    }
+}
