7b0a02de8463846da657c7202e9a8dff2776d254
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-12 16:40:55 +0400

    Add new method: DeleteCategory

--- ProductManagementApp/DataAccess/Helpers/CategoryDal.cs
+++ ProductManagementApp/DataAccess/Helpers/CategoryDal.cs
@@ -7,9 +7,11 @@ namespace ProductManagementApp.DataAccess.Helpers
     {
         public static void FillCategories(this DataTable table)
         {
-            using (IDbCommand command = Connection.CreateCommand("select id, name from categories;"))
+            using (IDbCommand command = Connection
+                .CreateCommand("select id, name from categories where deleted_date is null;"))
             {
                 IDataReader reader = command.ExecuteReader();
+                table.Clear();
                 table.Load(reader);
             }
         }
@@ -30,6 +32,17 @@ namespace ProductManagementApp.DataAccess.Helpers
             using (IDbCommand command = Connection
                 .CreateCommand("update categories set `name` = @name where id = @id;")
                 .AddParameter("@name", categoryName)
+                .AddParameter("@id", categoryId))
+            {
+                command.ExecuteNonQuery();
+                table.FillCategories();
+            }
+        }
+
+        public static void DeleteCategory(this DataTable table, int categoryId)
+        {
+            using (IDbCommand command = Connection
+                .CreateCommand("update categories set deleted_date = now() where id = @id;")
                 .AddParameter("@id", categoryId))
             {
                 command.ExecuteNonQuery();
