c8da2eb359fa2add10ef3e27b5e0dca229d0348f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-12 12:52:57 +0400

    Create exception: InvalidCategoryNameException

---
+++ ProductManagementApp/Exceptions/InvalidCategoryNameException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProductManagementApp.Exceptions
+{
+    public class InvalidCategoryNameException : Exception
+    {
+        public override string Message => "Invalid category name!";
+    }
+}
