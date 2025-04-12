f78c02c31887ab1741b6af00804a2532ff5eacc8
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-12 14:28:10 +0400

    Add new extension method: AddParemeter

---
+++ ProductManagementApp/AppCode/Extensions/DbExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+namespace ProductManagementApp.AppCode.Extensions
+{
+    public static partial class Extension
+    {
+        public static IDbCommand CreateCommand(this IDbConnection connection, string commandText)
+        {
+            IDbCommand command = connection.CreateCommand();
+            command.CommandText = commandText;
+
+            if (command.Connection.State != ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+
+            return command;
+        }
+
+        public static IDbCommand AddParameter(this IDbCommand command, string parameterName, object parameterValue)
+        {
+            IDbDataParameter parameter = command.CreateParameter();
+            parameter.ParameterName = parameterName;
+            parameter.Value = parameterValue;
+            command.Parameters.Add(parameter);
+            return command;
+        }
+    }
+}
