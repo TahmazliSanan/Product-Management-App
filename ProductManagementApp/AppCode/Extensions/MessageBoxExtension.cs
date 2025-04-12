1363de5f1ddaa3f6affef2840f9f7fc28f7a5f2f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-12 14:28:55 +0400

    Create new extension: MessageBoxExtension

---
+++ ProductManagementApp/AppCode/Extensions/MessageBoxExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace ProductManagementApp.AppCode.Extensions
+{
+    public static partial class Extension
+    {
+        public static void ShowInformation(string message)
+        {
+            MessageBox.Show(message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        
+        public static DialogResult ShowQuestion(string message)
+        {
+            return MessageBox.Show(message, "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+        }
+
+        public static void ShowWarning(string message)
+        {
+            MessageBox.Show(message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public static void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
