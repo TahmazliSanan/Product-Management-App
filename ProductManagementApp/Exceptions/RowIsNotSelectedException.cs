08f06a006d295e26d70cd02f58a19b085bb3dc81
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-12 14:29:29 +0400

    Create exception: RowIsNotSelectedException

---
+++ ProductManagementApp/Exceptions/RowIsNotSelectedException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProductManagementApp.Exceptions
+{
+    public class RowIsNotSelectedException : Exception
+    {
+        public override string Message => "Row is not selected!";
+    }
+}
