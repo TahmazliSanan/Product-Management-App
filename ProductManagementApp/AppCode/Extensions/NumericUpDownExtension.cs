8f7d9c4358cb51072183d81fb91810ad31f95ad1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-14 01:33:48 +0400

    Add new extension: NumericUpDownExtension

---
+++ ProductManagementApp/AppCode/Extensions/NumericUpDownExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace ProductManagementApp.AppCode.Extensions
+{
+    public static partial class Extension
+    {
+        public static void ConfigureNumericUpDownComponent(
+            this NumericUpDown numericUpDown)
+        {
+            numericUpDown.Maximum = int.MaxValue;
+        }
+    }
+}
